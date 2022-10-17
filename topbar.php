0ea512d915504e3ccf36f0d8ca1690321bf4aa66
Ismael Jose Escalante <ismael.escalante@outsourcearg.com>
2022-10-17 17:40:26 -0300

    cahnge the elementos topbar

--- topbar.php
+++ topbar.php
@@ -13,6 +13,17 @@
                     <div class="col-4">
                         <div class="top-bar-item">
                             <div class="top-bar-icon">
+                                <i class="flaticon-send-mail"></i>
+                            </div>
+                            <div class="top-bar-text">
+                                <h3>Email Us</h3>
+                                <p>inst.electricasdelnoroeste@gmail.com</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="top-bar-item">
+                            <div class="top-bar-icon">
                                 <i class="flaticon-calendar"></i>
                             </div>
                             <div class="top-bar-text">
@@ -29,17 +40,6 @@
                             <div class="top-bar-text">
                                 <h3>Comunicate:</h3>
                                 <p>+549 387 4232173</p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-4">
-                        <div class="top-bar-item">
-                            <div class="top-bar-icon">
-                                <i class="flaticon-send-mail"></i>
-                            </div>
-                            <div class="top-bar-text">
-                                <h3>Email Us</h3>
-                                <p>inst.electricasdelnoroeste@gmail.com</p>
                             </div>
                         </div>
                     </div>
